baeb12f507e9cf6c11991ba21e22c9214b98710f
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-01-14 10:05:27 +0500

    vdeo scroll responsive.

--- js/slider-data.js
+++ js/slider-data.js
@@ -1,50 +1,68 @@
 var anchor = $('.anchor').position().left;
-var mouseStill = false;
 var trigger = $('.videos');
 
+if($(window).width() < 768) {
+    function goLeft() {
+        trigger.animate({
+            scrollLeft: '-=75'
+        },100, goLeft);
+    }
 
-$(".left").mousedown(goLeft).mouseup(stopScroll);
-$(".right").mousedown(goRight).mouseup(stopScroll);
+    function goRight() {
+        trigger.animate({
+            scrollLeft: '+=75'
+        },100, goRight);
+    }
 
-function goLeft() {
+    $(".left").mousedown(goLeft).mouseup(stopScroll);
+    $(".right").mousedown(goRight).mouseup(stopScroll);
+}
+if($(window).width() > 768) {
+    function goLeft() {
         trigger.animate({
             scrollLeft: '-=150'
         },100, goLeft);
-}
+    }
 
-function goRight() {
+    function goRight() {
         trigger.animate({
             scrollLeft: '+=150'
         },100, goRight);
+    }
+
+    $(".left").mousedown(goLeft).mouseup(stopScroll);
+    $(".right").mousedown(goRight).mouseup(stopScroll);
 }
 
 function stopScroll() {
     trigger.stop();
 }
 
-    if($(window).width() < 769) {
-        $('video').removeAttr('disabled');
-        $('video').attr('controls', 'controls');
-        $('.overlay').css('display', 'none');
-    }
-    else {
-        $('video').attr('disabled', 'disabled');
-        $('video').removeAttr('controls');
-        $('.overlay').css('display', 'block');
 
-        $(".video-cont video").click(function() {
-            var src = $(this).children("source").attr("src");
-            var content = $(this).children("#content").attr("value");
-            var title = $(this).children("#title").attr("value");
-            var video = '<div><video class="embed-responsive-item" width="600px" height="400px" controls autoplay><source src="'+src+'" type="video/mp4"><source src="movie.ogg" type="video/ogg">Your browser does not support the video tag.</video></div>';
-            $("#myModal").modal();
-            $("#myModal").on("shown.bs.modal", function(){
-                $("#myModal .modal-body").html(video);
-                $("#myModal .modal-footer").html(content);
-                $("#myModal .modal-header h4").html(title);
-            });
-            $("#myModal").on("hidden.bs.modal", function(){
-                $("#myModal .modal-body").html('');
-            });
+
+if($(window).width() < 769) {
+    $('video').removeAttr('disabled');
+    $('video').attr('controls', 'controls');
+    $('.overlay').css('display', 'none');
+}
+else {
+    $('video').attr('disabled', 'disabled');
+    $('video').removeAttr('controls');
+    $('.overlay').css('display', 'block');
+
+    $(".video-cont video").click(function() {
+        var src = $(this).children("source").attr("src");
+        var content = $(this).children("#content").attr("value");
+        var title = $(this).children("#title").attr("value");
+        var video = '<div><video class="embed-responsive-item" width="600px" height="400px" controls autoplay><source src="'+src+'" type="video/mp4"><source src="movie.ogg" type="video/ogg">Your browser does not support the video tag.</video></div>';
+        $("#myModal").modal();
+        $("#myModal").on("shown.bs.modal", function(){
+            $("#myModal .modal-body").html(video);
+            $("#myModal .modal-footer").html(content);
+            $("#myModal .modal-header h4").html(title);
         });
-    }
+        $("#myModal").on("hidden.bs.modal", function(){
+            $("#myModal .modal-body").html('');
+        });
+    });
+}
