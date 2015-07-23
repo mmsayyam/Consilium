7c67ae86d3817a0a65eb03db7ad0f6d8fd1761f2
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-07-23 20:16:38 +0500

    Fonts, video icons, panel bg color updated

--- includes/video.php
+++ includes/video.php
@@ -1,4 +1,4 @@
-	<?php 
+	<?php
 		require_once 'connections/connection.php';
 
 		$query = "SELECT * FROM gallery WHERE g_type = 'video' ORDER BY g_id DESC LIMIT 5";
@@ -11,21 +11,21 @@
         	<div class="modal-content">
         		<div class="modal-header">
 			        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-		    	</div>   
-          		<div class="modal-body">                
+		    	</div>
+          		<div class="modal-body">
           		</div>
         	</div><!-- /.modal-content -->
       	</div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
 
-    
-    
+
+
     <div class="video-slider">
-		<span class="left"><img src="assets/img/Arrow_Right_Filled.svg"></span>
-		<span style="float:right" class="right"><img src="assets/img/Arrow_Right_Filled.svg"></span>
-    		
+		<span class="left"><img src="assets/img/video_left.png"></span>
+		<span style="float:right" class="right"><img src="assets/img/video_right.png"></span>
+
 		<ul class="videos">
-			
+
 			<?php $l = 0; do { $l++;?>
 				<li class="list-<?php echo $l ?>">
 					<div class="embed-responsive embed-responsive-4by3 video-cont">
@@ -34,15 +34,14 @@
 							<input type="hidden" value="<?php echo $row['g_content'] ?>" id="content">
 							<input type="hidden" value="<?php echo $row['g_title'] ?>" id="title">
 						</video>
-						<div class="overlay"><img src="assets/img/video_play_icon.svg"></div>
+						<div class="overlay"><img src="assets/img/video_play.png"></div>
 					</div>
 					<?php if ($l == 5) {  ?>
 						<div class="anchor"></div>
 					<?php } ?>
 				</li>
 			<?php } while($row = mysqli_fetch_array($result)) ?>
-					
-				
+
+
 		</ul>
 	</div>
-	
