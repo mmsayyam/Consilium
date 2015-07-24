e2e481850cf8a6a9471040d260adca02270bc9a6
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-07-25 00:16:53 +0500

    forgot to change connection

--- connections/connection.php
+++ connections/connection.php
@@ -1,4 +1,4 @@
 <?php
-		// $con = mysqli_connect('107.170.26.223', 'root', 'password', 'consultancy');
-	$con = mysqli_connect('localhost', 'root', '', 'consultancy');
+		$con = mysqli_connect('107.170.26.223', 'root', 'password', 'consultancy');
+	// $con = mysqli_connect('localhost', 'root', '', 'consultancy');
 ?>
